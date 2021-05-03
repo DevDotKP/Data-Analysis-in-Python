a31a2df66170b4f7f173147f52ff272e92fabb4c
DevDotKp <dev.kp271828@gmail.com>
2021-05-03 23:12:33 +0530

    Modifying arrays with NumPy

--- NumPyBasics/Basic01.py
+++ NumPyBasics/Basic01.py
@@ -21,3 +21,27 @@ print ( np.array ( nestedList ) )
 myMatrix = np.array ( (nestedList) )
 
 print ( myMatrix.shape )
+
+print ( np.arange ( 0, 10, 2 ) )
+
+print ( np.random.rand ( 3, 3 ) )  # Used to generate a random array or matrix (2D array)
+
+print ( np.random.normal ( 150, 35, 9 ) )  # Generates 9 numbers for a dataset with  the mean of 150 and std.dev of  35
+
+print ( np.random.randint ( 10, 15, 10 ) )
+
+np.random.seed (
+    101 )  # Used to generate the same random numbers in different computers. The next line will have the same O/P in every single computer
+print ( np.random.rand ( 4 ) )
+
+arbitraryArray = np.arange ( 0, 100, 4 )
+
+print ( arbitraryArray )  # As expected, prints 25 values as 1D array
+
+arbitraryArray = arbitraryArray.reshape ( 5, 5 )  # Reshapes the 1D array to a 5*5 array
+
+print ( arbitraryArray )  # Prints the values as 5*5 array
+
+print ( arbitraryArray.min () )
+
+print ( arbitraryArray.max () )
