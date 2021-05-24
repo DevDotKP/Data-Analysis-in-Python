786a995fca52025f5623939f03bc29e0ca629e13
DevDotKp <dev.kp271828@gmail.com>
2021-05-24 20:02:24 +0530

    Added constants

--- Constants.py
+++ Constants.py
@@ -1,2 +1,7 @@
 USD = 'USD($)'
 TIME = 'Time'
+STARBUCKS = 'https://github.com/DevDotKP/Data-Analysis-in-Python/blob/Test/Pandas/TimeSeries/starbucks.csv'
+HEADER = {
+    "User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36",
+    "X-Requested-With" : "XMLHttpRequest"
+}
--- Pandas/TimeSeries/TimeResampling.py
+++ Pandas/TimeSeries/TimeResampling.py
@@ -4,13 +4,9 @@ import requests
 
 import Constants  # Using the defined constants
 
-url = 'https://github.com/DevDotKP/Data-Analysis-in-Python/blob/Test/Pandas/TimeSeries/starbucks.csv'
+url = Constants.STARBUCKS
 
-header = {
-    "User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36",
-    "X-Requested-With" : "XMLHttpRequest"
-}
-r = requests.get ( url=url, headers=header )
+r = requests.get ( url=url, headers=Constants.HEADER )
 starbucksDF = pd.read_html ( r.text, index_col='Date',
                              parse_dates=True )  # With 'parse_dates=True', we are informing Python/Pandas that the index column has DateTime values
 starbucksDF = starbucksDF[0]
