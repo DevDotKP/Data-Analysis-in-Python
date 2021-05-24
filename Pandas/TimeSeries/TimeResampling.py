306c4dfefd932c5c0d000be64096101c8e509ba9
DevDotKp <dev.kp271828@gmail.com>
2021-05-24 19:15:05 +0530

    Time resampling basics

--- Pandas/TimeSeries/TimeResampling.py
+++ Pandas/TimeSeries/TimeResampling.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import requests
 
@@ -15,3 +16,16 @@ starbucksDF = starbucksDF.drop ( 'Unnamed: 0',
                                  axis=1 )  # Removing the unnamed:0 column. It should normally not appear after you define an index column, like we've done, but was not removed in this case. Warrants a research on the issue
 
 # https://github.com/DevDotKP/Data-Analysis-in-Python/blob/Test/Pandas/TimeSeries/01-Time-Resampling.ipynb can be used to check for more frequencies
+
+print ( starbucksDF.resample ( rule='A' ).mean () )  # Converting business week data to yearly data
+
+ax = starbucksDF['Close'].resample ( 'A' ).mean ().plot.bar ( title='Average mean closing price',
+                                                              grid=True )  # Shows the average closing price AT THE END of each year
+ax.set ( xlabel='Year', ylabel='USD $' )
+plt.show ()
+
+title = 'Max monthly closing price for SBUX'
+ax = starbucksDF['Close'].resample ( rule='M' ).max ().plot.line ( color=(0.52, 0.32, 0.31), title=title,
+                                                                   grid=True )  # Giving color values from (r,g,b) that range from [0,1]
+ax.set ( xlabel='Month', ylabel='USD $' )
+plt.show ()
